e8a9e729e7466843324e40faa521bbaf7cd4c5e9
Leonardo Rodrigues <59567995+leorjr@users.noreply.github.com>
2024-06-23 11:45:37 -0300

    feat: implementado validação do body na criação de uma entrega

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import BaseError from "../errors/baseError";
+import ValidationError from "../errors/validationError";
 import IGenericResponseDto from "../types/IgenericResponseDto";
 
 const errorHandler = (
@@ -18,6 +19,17 @@ const errorHandler = (
 
         return res.status(response.status).json(response)
     }
+
+    if(error instanceof ValidationError){
+		const response: IGenericResponseDto = {
+			status: error.status,
+			success: false,
+			message: error.message,
+			errors: error.errors
+		};
+
+		return res.status(error.status).json(response);
+	}
 
     console.log(error)
 
